864a850066d59f5153f80c30aab4969cda051470
ACGaming <4818419+ACGaming@users.noreply.github.com>
2024-06-06 17:07:59 +0200

    Increase crucible levels

--- src/main/java/mod/emt/harkenscythe/blocks/HSBloodCrucible.java
+++ src/main/java/mod/emt/harkenscythe/blocks/HSBloodCrucible.java
@@ -1,9 +1,17 @@
 package mod.emt.harkenscythe.blocks;
 
+import java.util.List;
 import java.util.Random;
+import javax.annotation.Nullable;
+import mod.emt.harkenscythe.HarkenScythe;
 import mod.emt.harkenscythe.init.HSItems;
 import mod.emt.harkenscythe.items.HSEssenceKeeper;
-import net.minecraft.block.BlockCauldron;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyInteger;
+import net.minecraft.block.state.BlockFaceShape;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
@@ -14,20 +22,98 @@ import net.minecraft.stats.StatList;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-public class HSBloodCrucible extends BlockCauldron
+@SuppressWarnings("deprecation")
+public class HSBloodCrucible extends Block
 {
+    public static final PropertyInteger LEVEL = PropertyInteger.create("level", 0, 10);
+    protected static final AxisAlignedBB AABB_LEGS = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.3125D, 1.0D);
+    protected static final AxisAlignedBB AABB_WALL_NORTH = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 0.125D);
+    protected static final AxisAlignedBB AABB_WALL_SOUTH = new AxisAlignedBB(0.0D, 0.0D, 0.875D, 1.0D, 1.0D, 1.0D);
+    protected static final AxisAlignedBB AABB_WALL_EAST = new AxisAlignedBB(0.875D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D);
+    protected static final AxisAlignedBB AABB_WALL_WEST = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 0.125D, 1.0D, 1.0D);
+
     public HSBloodCrucible()
     {
-        super();
+        super(Material.IRON, MapColor.RED);
+        setCreativeTab(HarkenScythe.TAB);
+        setDefaultState(blockState.getBaseState().withProperty(LEVEL, 0));
+    }
+
+    public void setLevel(World world, BlockPos pos, IBlockState state, int level)
+    {
+        world.setBlockState(pos, state.withProperty(LEVEL, MathHelper.clamp(level, 0, 10)), 2);
+        world.updateComparatorOutputLevel(pos, this);
     }
 
     @Override
-    public void onEntityCollision(World worldIn, BlockPos pos, IBlockState state, Entity entityIn)
+    public IBlockState getStateFromMeta(int meta)
     {
+        return this.getDefaultState().withProperty(LEVEL, meta);
+    }
 
+    @Override
+    public int getMetaFromState(IBlockState state)
+    {
+        return state.getValue(LEVEL);
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isPassable(IBlockAccess world, BlockPos pos)
+    {
+        return true;
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        return FULL_BLOCK_AABB;
+    }
+
+    @Override
+    public BlockFaceShape getBlockFaceShape(IBlockAccess world, IBlockState state, BlockPos pos, EnumFacing face)
+    {
+        if (face == EnumFacing.UP)
+        {
+            return BlockFaceShape.BOWL;
+        }
+        else
+        {
+            return face == EnumFacing.DOWN ? BlockFaceShape.UNDEFINED : BlockFaceShape.SOLID;
+        }
+    }
+
+    @Override
+    public void addCollisionBoxToList(IBlockState state, World world, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, @Nullable Entity entity, boolean isActualState)
+    {
+        addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_LEGS);
+        addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_WALL_WEST);
+        addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_WALL_NORTH);
+        addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_WALL_EAST);
+        addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_WALL_SOUTH);
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune)
+    {
+        return Item.getItemFromBlock(this);
     }
 
     @Override
@@ -42,7 +128,7 @@ public class HSBloodCrucible extends BlockCauldron
         if (!(item instanceof HSEssenceKeeper)) return false;
 
         int level = state.getValue(LEVEL);
-        if (level < 3 && !player.isSneaking() && (item == HSItems.essence_keeper_blood || item == HSItems.essence_vessel_blood))
+        if (level < 10 && !player.isSneaking() && (item == HSItems.essence_keeper_blood || item == HSItems.essence_vessel_blood))
         {
             fillCrucible(world, pos, state, player, hand, stack, item, level);
         }
@@ -55,30 +141,36 @@ public class HSBloodCrucible extends BlockCauldron
     }
 
     @Override
-    public void fillWithRain(World world, BlockPos pos)
-    {
-
-    }
-
-    @Override
-    public Item getItemDropped(IBlockState state, Random rand, int fortune)
-    {
-        return Item.getItemFromBlock(this);
-    }
-
-    @Override
     public ItemStack getItem(World world, BlockPos pos, IBlockState state)
     {
         return new ItemStack(Item.getItemFromBlock(this));
+    }
+
+    @Override
+    public boolean hasComparatorInputOverride(IBlockState state)
+    {
+        return true;
+    }
+
+    @Override
+    public int getComparatorInputOverride(IBlockState blockState, World world, BlockPos pos)
+    {
+        return blockState.getValue(LEVEL);
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, LEVEL);
     }
 
     private void fillCrucible(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, ItemStack stack, Item item, int level)
     {
         if (!player.capabilities.isCreativeMode)
         {
-            if (stack.getItemDamage() + 20 < stack.getMaxDamage())
+            if (stack.getItemDamage() + 10 < stack.getMaxDamage())
             {
-                stack.setItemDamage(stack.getItemDamage() + 20);
+                stack.setItemDamage(stack.getItemDamage() + 10);
             }
             else
             {
@@ -94,7 +186,7 @@ public class HSBloodCrucible extends BlockCauldron
             }
         }
         world.playSound(null, pos, SoundEvents.ITEM_BOTTLE_EMPTY, SoundCategory.BLOCKS, 1.0F, 1.0F);
-        setWaterLevel(world, pos, state, level + 1);
+        setLevel(world, pos, state, level + 1);
         player.addStat(StatList.getObjectUseStats(item));
     }
 
@@ -106,10 +198,7 @@ public class HSBloodCrucible extends BlockCauldron
             {
                 stack.shrink(1);
                 ItemStack newStack = item == HSItems.essence_keeper ? new ItemStack(HSItems.essence_keeper_blood) : new ItemStack(HSItems.essence_vessel_blood);
-                if (item == HSItems.essence_vessel)
-                {
-                    newStack.setItemDamage(newStack.getMaxDamage() - 20);
-                }
+                newStack.setItemDamage(newStack.getMaxDamage() - 10);
                 player.setHeldItem(hand, newStack);
             }
             else if (item == HSItems.essence_keeper_blood || item == HSItems.essence_vessel_blood)
@@ -117,7 +206,7 @@ public class HSBloodCrucible extends BlockCauldron
                 if (stack.getItemDamage() == 0) return;
                 if (stack.getItemDamage() > 0)
                 {
-                    stack.setItemDamage(stack.getItemDamage() - 20);
+                    stack.setItemDamage(stack.getItemDamage() - 10);
                 }
                 if (stack.getItemDamage() <= 0)
                 {
@@ -128,7 +217,7 @@ public class HSBloodCrucible extends BlockCauldron
             }
         }
         world.playSound(null, pos, SoundEvents.ITEM_BOTTLE_FILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
-        setWaterLevel(world, pos, state, level - 1);
+        setLevel(world, pos, state, level - 1);
         player.addStat(StatList.getObjectUseStats(item));
     }
 }
