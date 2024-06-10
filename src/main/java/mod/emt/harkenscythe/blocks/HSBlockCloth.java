55c2fdb9a20403f78b21dacc58e6c7daa42acc94
Me <135455255+IcarussOne@users.noreply.github.com>
2024-06-10 03:44:09 -0500

    Update cloth blocks

--- src/main/java/mod/emt/harkenscythe/blocks/HSBlockCloth.java
+++ src/main/java/mod/emt/harkenscythe/blocks/HSBlockCloth.java
@@ -1,15 +1,30 @@
 package mod.emt.harkenscythe.blocks;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import com.google.common.collect.Lists;
+
 import mod.emt.harkenscythe.init.HSBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemShears;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stats.StatList;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.IShearable;
 
-public class HSBlockCloth extends Block
+public class HSBlockCloth extends Block implements IShearable
 {
     public HSBlockCloth(MapColor mapColor)
     {
@@ -23,4 +38,30 @@ public class HSBlockCloth extends Block
     {
         return this == HSBlocks.bloodweave_cloth || this == HSBlocks.soulweave_cloth;
     }
+    
+    @Override
+    public void harvestBlock(World worldIn, EntityPlayer player, BlockPos pos, IBlockState state, @Nullable TileEntity te, ItemStack stack)
+    {
+        if (!worldIn.isRemote && stack.getItem() instanceof ItemShears)
+        {
+            player.addStat(StatList.getBlockStats(this));
+            spawnAsEntity(worldIn, pos, new ItemStack(Item.getItemFromBlock(this), 1));
+        }
+        else
+        {
+            super.harvestBlock(worldIn, player, pos, state, te, stack);
+        }
+    }
+
+	@Override
+	public boolean isShearable(ItemStack item, IBlockAccess world, BlockPos pos)
+	{
+		return true;
+	}
+
+	@Override
+	public List<ItemStack> onSheared(ItemStack item, IBlockAccess world, BlockPos pos, int fortune)
+	{
+		return Lists.newArrayList(new ItemStack(Item.getItemFromBlock(this)));
+	}
 }
