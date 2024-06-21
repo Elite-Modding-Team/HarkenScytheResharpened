d7f1927dc6f5a5e8fca8c64fd4e89d1da0cce613
Me <135455255+IcarussOne@users.noreply.github.com>
2024-06-21 00:17:36 -0500

    Add loot table

---
+++ src/main/java/mod/emt/harkenscythe/init/HSLootTables.java
@@ -0,0 +1,9 @@
+package mod.emt.harkenscythe.init;
+
+import mod.emt.harkenscythe.HarkenScythe;
+import net.minecraft.util.ResourceLocation;
+
+public class HSLootTables
+{
+    public static final ResourceLocation HARBINGER = new ResourceLocation(HarkenScythe.MOD_ID, "entities/harbinger");
+}
