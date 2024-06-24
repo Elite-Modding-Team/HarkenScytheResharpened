49397d6505bedf1a1297c8e7df3fde4de6071dfd
ACGaming <4818419+ACGaming@users.noreply.github.com>
2024-06-24 11:50:38 +0200

    Adjust harbinger transparency

--- src/main/java/mod/emt/harkenscythe/client/renderers/RenderHSEntityHarbinger.java
+++ src/main/java/mod/emt/harkenscythe/client/renderers/RenderHSEntityHarbinger.java
@@ -4,6 +4,7 @@ import javax.annotation.Nullable;
 import mod.emt.harkenscythe.HarkenScythe;
 import mod.emt.harkenscythe.client.models.ModelHSHarbinger;
 import mod.emt.harkenscythe.entities.HSEntityHarbinger;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.client.renderer.entity.RenderManager;
@@ -15,18 +16,34 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class RenderHSEntityHarbinger extends RenderBiped<HSEntityHarbinger>
 {
-    public static final ResourceLocation TEXTURES = new ResourceLocation(HarkenScythe.MOD_ID, "textures/entities/harbinger.png");
+    public static final ResourceLocation HARBINGER_TEXTURES = new ResourceLocation(HarkenScythe.MOD_ID, "textures/entities/harbinger.png");
 
-    public RenderHSEntityHarbinger(RenderManager renderManagerIn)
+    public RenderHSEntityHarbinger(RenderManager renderManager)
     {
-        super(renderManagerIn, new ModelHSHarbinger(), 0.0F);
+        super(renderManager, new ModelHSHarbinger(), 0.0F);
     }
 
     @Nullable
     @Override
     protected ResourceLocation getEntityTexture(HSEntityHarbinger entity)
     {
-        return TEXTURES;
+        return HARBINGER_TEXTURES;
+    }
+
+    @Override
+    protected void renderModel(HSEntityHarbinger entityHarbinger, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor)
+    {
+        super.renderModel(entityHarbinger, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor);
+        GlStateManager.disableBlend();
+    }
+
+    @Override
+    protected void preRenderCallback(HSEntityHarbinger entityHarbinger, float partialTickTime)
+    {
+        super.preRenderCallback(entityHarbinger, partialTickTime);
+        GlStateManager.enableBlend();
+        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 0.9F);
     }
 
     public static class Factory implements IRenderFactory<HSEntityHarbinger>
