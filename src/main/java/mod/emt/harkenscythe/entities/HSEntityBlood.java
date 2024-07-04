f6c5fb4e52c7017ee76bcb1a3caad34568a0fc6c
ACGaming <4818419+ACGaming@users.noreply.github.com>
2024-07-04 16:41:32 +0200

    Apply fill level dependent pitches

--- src/main/java/mod/emt/harkenscythe/entities/HSEntityBlood.java
+++ src/main/java/mod/emt/harkenscythe/entities/HSEntityBlood.java
@@ -29,7 +29,9 @@ public class HSEntityBlood extends HSEntityEssence
             ItemStack newStack = item == HSItems.essence_keeper ? new ItemStack(HSItems.essence_keeper_blood) : new ItemStack(HSItems.essence_vessel_blood);
             newStack.setItemDamage(newStack.getMaxDamage() - 1);
             player.setHeldItem(hand, newStack);
-            this.world.playSound(null, player.getPosition(), SoundEvents.ITEM_BOTTLE_FILL_DRAGONBREATH, SoundCategory.PLAYERS, 1.0F, 1.5F / (world.rand.nextFloat() * 0.4F + 1.2F));
+            float pitch = newStack.getItemDamage() == 0 ? 1.0F : 1.0F - ((float) newStack.getItemDamage() / newStack.getMaxDamage() * 0.5F);
+            if (newStack.getItem() == HSItems.essence_keeper_blood) pitch += 0.5F;
+            this.world.playSound(null, player.getPosition(), SoundEvents.ITEM_BOTTLE_FILL_DRAGONBREATH, SoundCategory.PLAYERS, 1.0F, pitch);
             this.world.spawnParticle(EnumParticleTypes.CLOUD, this.posX, this.posY + 1.5D, this.posZ, 0.0D, 0.1D, 0.0D);
             this.setDead();
         }
@@ -46,7 +48,9 @@ public class HSEntityBlood extends HSEntityEssence
                 ItemStack newStack = item == HSItems.essence_keeper_blood ? new ItemStack(HSItems.essence_keeper_blood) : new ItemStack(HSItems.essence_vessel_blood);
                 player.setHeldItem(hand, newStack);
             }
-            this.world.playSound(null, player.getPosition(), SoundEvents.ITEM_BOTTLE_FILL_DRAGONBREATH, SoundCategory.PLAYERS, 1.0F, 1.5F / (world.rand.nextFloat() * 0.4F + 1.2F));
+            float pitch = stack.getItemDamage() == 0 ? 1.0F : 1.0F - ((float) stack.getItemDamage() / stack.getMaxDamage() * 0.5F);
+            if (stack.getItem() == HSItems.essence_keeper_blood) pitch += 0.5F;
+            this.world.playSound(null, player.getPosition(), SoundEvents.ITEM_BOTTLE_FILL_DRAGONBREATH, SoundCategory.PLAYERS, 1.0F, pitch);
             this.world.spawnParticle(EnumParticleTypes.CLOUD, this.posX, this.posY + 1.5D, this.posZ, 0.0D, 0.1D, 0.0D);
             this.setDead();
         }
