abc860a56a6e96210a4ded698e8fc142e01e8663
ACGaming <4818419+ACGaming@users.noreply.github.com>
2024-07-24 09:38:02 +0200

    Clarify repair recipes, add tooltips

--- src/main/java/mod/emt/harkenscythe/compat/jei/blood/HSJEIRecipeWrapperBloodAltar.java
+++ src/main/java/mod/emt/harkenscythe/compat/jei/blood/HSJEIRecipeWrapperBloodAltar.java
@@ -1,14 +1,22 @@
 package mod.emt.harkenscythe.compat.jei.blood;
 
+import java.util.ArrayList;
+import java.util.List;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.IRecipeWrapper;
+import mod.emt.harkenscythe.HarkenScythe;
 import mod.emt.harkenscythe.init.HSItems;
 import mod.emt.harkenscythe.recipe.BloodAltarRecipe;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 
 public class HSJEIRecipeWrapperBloodAltar implements IRecipeWrapper
 {
+    private static final ResourceLocation REPAIR_ICON = new ResourceLocation(HarkenScythe.MOD_ID, "textures/gui/repair.png");
     private final BloodAltarRecipe recipe;
 
     public HSJEIRecipeWrapperBloodAltar(BloodAltarRecipe recipe)
@@ -19,7 +27,7 @@ public class HSJEIRecipeWrapperBloodAltar implements IRecipeWrapper
     @Override
     public void getIngredients(IIngredients ingredients)
     {
-        ingredients.setInput(ItemStack.class, recipe.getInput());
+        ingredients.setInput(ItemStack.class, getInputStack());
         ingredients.setOutput(ItemStack.class, recipe.getOutput());
     }
 
@@ -37,5 +45,47 @@ public class HSJEIRecipeWrapperBloodAltar implements IRecipeWrapper
         int x = 40 - stringWidth / 2;
         int y = 44;
         minecraft.fontRenderer.drawString(bloodCosts, x, y, 0xFF5555, true);
+
+        if (isRepairRecipe())
+        {
+            GlStateManager.enableBlend();
+            minecraft.getTextureManager().bindTexture(REPAIR_ICON);
+            Gui.drawModalRectWithCustomSizedTexture(62, 14, 0, 0, 16, 16, 16, 16);
+            GlStateManager.disableBlend();
+        }
+    }
+
+    @Override
+    public List<String> getTooltipStrings(int mouseX, int mouseY)
+    {
+        List<String> tooltips = new ArrayList<>();
+        if (mouseX >= 32 && mouseX < 48 && mouseY >= 28 && mouseY < 44)
+        {
+            tooltips.add(I18n.format("item.harkenscythe.blood_essence.name"));
+        }
+        if (mouseX >= 64 && mouseX < 80 && mouseY >= 30 && mouseY < 46)
+        {
+            tooltips.add(I18n.format("item.harkenscythe.harken_athame.name"));
+        }
+        if (isRepairRecipe() && mouseX >= 62 && mouseX < 78 && mouseY >= 14 && mouseY < 30)
+        {
+            tooltips.add(I18n.format("tooltip.harkenscythe.repair"));
+        }
+        return tooltips;
+    }
+
+    private boolean isRepairRecipe()
+    {
+        return recipe.getInput().getItem() == recipe.getOutput().getItem();
+    }
+
+    private ItemStack getInputStack()
+    {
+        ItemStack inputStack = recipe.getInput().copy();
+        if (isRepairRecipe())
+        {
+            inputStack.setItemDamage(recipe.getRequiredBlood());
+        }
+        return inputStack;
     }
 }
--- src/main/java/mod/emt/harkenscythe/compat/jei/soul/HSJEIRecipeWrapperSoulAltar.java
+++ src/main/java/mod/emt/harkenscythe/compat/jei/soul/HSJEIRecipeWrapperSoulAltar.java
@@ -1,14 +1,22 @@
 package mod.emt.harkenscythe.compat.jei.soul;
 
+import java.util.ArrayList;
+import java.util.List;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.IRecipeWrapper;
+import mod.emt.harkenscythe.HarkenScythe;
 import mod.emt.harkenscythe.init.HSItems;
 import mod.emt.harkenscythe.recipe.SoulAltarRecipe;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 
 public class HSJEIRecipeWrapperSoulAltar implements IRecipeWrapper
 {
+    private static final ResourceLocation REPAIR_ICON = new ResourceLocation(HarkenScythe.MOD_ID, "textures/gui/repair.png");
     private final SoulAltarRecipe recipe;
 
     public HSJEIRecipeWrapperSoulAltar(SoulAltarRecipe recipe)
@@ -19,7 +27,7 @@ public class HSJEIRecipeWrapperSoulAltar implements IRecipeWrapper
     @Override
     public void getIngredients(IIngredients ingredients)
     {
-        ingredients.setInput(ItemStack.class, recipe.getInput());
+        ingredients.setInput(ItemStack.class, getInputStack());
         ingredients.setOutput(ItemStack.class, recipe.getOutput());
     }
 
@@ -37,5 +45,47 @@ public class HSJEIRecipeWrapperSoulAltar implements IRecipeWrapper
         int x = 40 - stringWidth / 2;
         int y = 44;
         minecraft.fontRenderer.drawString(soulCosts, x, y, 0x55FFFF, true);
+
+        if (isRepairRecipe())
+        {
+            GlStateManager.enableBlend();
+            minecraft.getTextureManager().bindTexture(REPAIR_ICON);
+            Gui.drawModalRectWithCustomSizedTexture(62, 14, 0, 0, 16, 16, 16, 16);
+            GlStateManager.disableBlend();
+        }
+    }
+
+    @Override
+    public List<String> getTooltipStrings(int mouseX, int mouseY)
+    {
+        List<String> tooltips = new ArrayList<>();
+        if (mouseX >= 32 && mouseX < 48 && mouseY >= 28 && mouseY < 44)
+        {
+            tooltips.add(I18n.format("item.harkenscythe.soul_essence.name"));
+        }
+        if (mouseX >= 64 && mouseX < 80 && mouseY >= 30 && mouseY < 46)
+        {
+            tooltips.add(I18n.format("item.harkenscythe.harken_athame.name"));
+        }
+        if (isRepairRecipe() && mouseX >= 62 && mouseX < 78 && mouseY >= 14 && mouseY < 30)
+        {
+            tooltips.add(I18n.format("tooltip.harkenscythe.repair"));
+        }
+        return tooltips;
+    }
+
+    private boolean isRepairRecipe()
+    {
+        return recipe.getInput().getItem() == recipe.getOutput().getItem();
+    }
+
+    private ItemStack getInputStack()
+    {
+        ItemStack inputStack = recipe.getInput().copy();
+        if (isRepairRecipe())
+        {
+            inputStack.setItemDamage(recipe.getRequiredSouls());
+        }
+        return inputStack;
     }
 }
