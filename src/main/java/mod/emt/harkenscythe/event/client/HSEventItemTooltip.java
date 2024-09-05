cc7e862e6223c39f39ffd3c1dd5a65fa8d9039aa
Me <135455255+IcarussOne@users.noreply.github.com>
2024-09-05 04:57:28 -0500

    Add lots of more tooltips

--- src/main/java/mod/emt/harkenscythe/event/client/HSEventItemTooltip.java
+++ src/main/java/mod/emt/harkenscythe/event/client/HSEventItemTooltip.java
@@ -22,15 +22,25 @@ public class HSEventItemTooltip
 
     private static final ResourceLocation ANCIENT_NECRONOMICON = new ResourceLocation(HarkenScythe.MOD_ID, "ancient_necronomicon");
     private static final ResourceLocation ANCIENT_NECRONOMICON_PAGE = new ResourceLocation(HarkenScythe.MOD_ID, "ancient_necronomicon_page");
+    private static final ResourceLocation BIOMASS = new ResourceLocation(HarkenScythe.MOD_ID, "biomass");
     private static final ResourceLocation BIOMASS_SEED = new ResourceLocation(HarkenScythe.MOD_ID, "biomass_seed");
+    private static final ResourceLocation BLOOD_ESSENCE = new ResourceLocation(HarkenScythe.MOD_ID, "blood_essence");
     private static final ResourceLocation BLUNT_HARKEN_BLADE = new ResourceLocation(HarkenScythe.MOD_ID, "blunt_harken_blade");
     private static final ResourceLocation CARNAGE_BOOK = new ResourceLocation(HarkenScythe.MOD_ID, "carnage_book");
+    private static final ResourceLocation CREEPBALL = new ResourceLocation(HarkenScythe.MOD_ID, "creepball");
+    private static final ResourceLocation DIMENSIONAL_MIRROR = new ResourceLocation(HarkenScythe.MOD_ID, "dimensional_mirror");
+    private static final ResourceLocation GERMINATED_BIOMASS_SEED = new ResourceLocation(HarkenScythe.MOD_ID, "germinated_biomass_seed");
     private static final ResourceLocation HARKEN_ATHAME = new ResourceLocation(HarkenScythe.MOD_ID, "harken_athame");
+    private static final ResourceLocation LIVINGMETAL_INGOT = new ResourceLocation(HarkenScythe.MOD_ID, "livingmetal_ingot");
     private static final ResourceLocation LIVINGMETAL_SHEARS = new ResourceLocation(HarkenScythe.MOD_ID, "livingmetal_shears");
     private static final ResourceLocation REAPER_SCYTHE = new ResourceLocation(HarkenScythe.MOD_ID, "reaper_scythe");
     private static final ResourceLocation SHADOW_BOOK = new ResourceLocation(HarkenScythe.MOD_ID, "shadow_book");
     private static final ResourceLocation SOUL_CAKE = new ResourceLocation(HarkenScythe.MOD_ID, "soul_cake");
     private static final ResourceLocation SOUL_COOKIE = new ResourceLocation(HarkenScythe.MOD_ID, "soul_cookie");
+    private static final ResourceLocation SOUL_ESSENCE = new ResourceLocation(HarkenScythe.MOD_ID, "soul_essence");
+    private static final ResourceLocation SPECTRAL_POTION_AFFLICTION = new ResourceLocation(HarkenScythe.MOD_ID, "spectral_potion_affliction");
+    private static final ResourceLocation SPECTRAL_POTION_FLAME = new ResourceLocation(HarkenScythe.MOD_ID, "spectral_potion_flame");
+    private static final ResourceLocation SPECTRAL_POTION_PURIFYING = new ResourceLocation(HarkenScythe.MOD_ID, "spectral_potion_purifying");
 
     @SubscribeEvent
     public static void onItemTooltip(ItemTooltipEvent event)
@@ -62,9 +72,17 @@ public class HSEventItemTooltip
             event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.ancient_necronomicon_page.2"));
             event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.ancient_necronomicon_page.1"));
         }
+        else if (ForgeRegistries.ITEMS.getKey(item).equals(BIOMASS))
+        {
+            event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.biomass"));
+        }
         else if (ForgeRegistries.ITEMS.getKey(item).equals(BIOMASS_SEED))
         {
             event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.biomass_seed"));
+        }
+        else if (ForgeRegistries.ITEMS.getKey(item).equals(BLOOD_ESSENCE))
+        {
+            event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.blood_essence"));
         }
         else if (ForgeRegistries.ITEMS.getKey(item).equals(BLUNT_HARKEN_BLADE))
         {
@@ -75,13 +93,31 @@ public class HSEventItemTooltip
             event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.carnage_book.2"));
             event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.carnage_book.1"));
         }
+        else if (ForgeRegistries.ITEMS.getKey(item).equals(CREEPBALL))
+        {
+            event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.creepball"));
+        }
+        else if (ForgeRegistries.ITEMS.getKey(item).equals(DIMENSIONAL_MIRROR))
+        {
+            event.getToolTip().add(1, "");
+            event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.dimensional_mirror"));
+        }
+        else if (ForgeRegistries.ITEMS.getKey(item).equals(GERMINATED_BIOMASS_SEED))
+        {
+            event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.germinated_biomass_seed"));
+        }
         else if (ForgeRegistries.ITEMS.getKey(item).equals(HARKEN_ATHAME))
         {
             event.getToolTip().add(1, "");
             event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.athame"));
         }
+        else if (ForgeRegistries.ITEMS.getKey(item).equals(LIVINGMETAL_INGOT))
+        {
+            event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.livingmetal_ingot"));
+        }
         else if (ForgeRegistries.ITEMS.getKey(item).equals(LIVINGMETAL_SHEARS))
         {
+            event.getToolTip().add(1, "");
             event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.shears"));
         }
         else if (ForgeRegistries.ITEMS.getKey(item).equals(REAPER_SCYTHE))
@@ -104,6 +140,22 @@ public class HSEventItemTooltip
         else if (ForgeRegistries.ITEMS.getKey(item).equals(SOUL_COOKIE))
         {
             event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.soul_cookie"));
+        }
+        else if (ForgeRegistries.ITEMS.getKey(item).equals(SOUL_ESSENCE))
+        {
+            event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.soul_essence"));
+        }
+        else if (ForgeRegistries.ITEMS.getKey(item).equals(SPECTRAL_POTION_AFFLICTION))
+        {
+            event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.spectral_potion_affliction"));
+        }
+        else if (ForgeRegistries.ITEMS.getKey(item).equals(SPECTRAL_POTION_FLAME))
+        {
+            event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.spectral_potion_flame"));
+        }
+        else if (ForgeRegistries.ITEMS.getKey(item).equals(SPECTRAL_POTION_PURIFYING))
+        {
+            event.getToolTip().add(1, I18n.format("tooltip.harkenscythe.spectral_potion_purifying"));
         }
     }
 }
