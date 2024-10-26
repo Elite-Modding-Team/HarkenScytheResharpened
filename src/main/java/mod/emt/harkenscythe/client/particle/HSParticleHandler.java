12ae2973b22c9bb0f590bab93990fbea57bd294b
Me <135455255+IcarussOne@users.noreply.github.com>
2024-10-26 04:13:42 -0500

    Add particle handler

---
+++ src/main/java/mod/emt/harkenscythe/client/particle/HSParticleHandler.java
@@ -0,0 +1,47 @@
+package mod.emt.harkenscythe.client.particle;
+
+import java.awt.Color;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.relauncher.Side;
+
+@EventBusSubscriber(Side.CLIENT)
+public class HSParticleHandler
+{
+    public static void spawnColoredParticle(EnumParticleTypes type, double x, double y, double z, Color color, double velX, double velY, double velZ)
+    {
+        Particle particle = Minecraft.getMinecraft().effectRenderer.spawnEffectParticle(type.getParticleID(), x, y, z, velX, velY, velZ);
+        float randBrightness = 0.5F + (float) Math.random();
+
+        particle.setRBGColorF((color.getRed() / 255.0F) * randBrightness, (color.getGreen() / 255.0F) * randBrightness, (color.getBlue() / 255.0F) * randBrightness);
+    }
+
+    public static void spawnDarkColoredParticle(EnumParticleTypes type, double x, double y, double z, Color color, double velX, double velY, double velZ)
+    {
+        Particle particle = Minecraft.getMinecraft().effectRenderer.spawnEffectParticle(type.getParticleID(), x, y, z, velX, velY, velZ);
+
+        particle.setRBGColorF((color.getRed() / 255.0F), (color.getGreen() / 255.0F), (color.getBlue() / 255.0F));
+    }
+
+    public static void spawnBeamParticles(EnumParticleTypes type, World world, double sourceX, double sourceY, double sourceZ, Color color, double targetX, double targetY, double targetZ)
+    {
+        int particleAmount = 40;
+
+        for (int i = 0; i < particleAmount; i++)
+        {
+            double beam = i / (particleAmount - 1.0D);
+            float velX = 1.0F;
+            float velY = 0.5F;
+            float velZ = 0.5F;
+            double x = sourceX + (targetX - sourceX) * beam + world.rand.nextGaussian() * 0.005;
+            double y = sourceY + (targetY - sourceY) * beam + world.rand.nextGaussian() * 0.005;
+            double z = sourceZ + (targetZ - sourceZ) * beam + world.rand.nextGaussian() * 0.005;
+            
+            spawnColoredParticle(type, x, y, z, color, velX, velY, velZ);
+        }
+    }
+}
