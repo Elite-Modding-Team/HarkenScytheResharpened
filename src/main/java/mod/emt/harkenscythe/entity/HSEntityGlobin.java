1bd0e811288b9549ce66ce949c24e7acd000d01a
Me <135455255+IcarussOne@users.noreply.github.com>
2024-12-09 23:04:21 -0600

    Remove hardcoded slimeball drop

--- src/main/java/mod/emt/harkenscythe/entity/HSEntityGlobin.java
+++ src/main/java/mod/emt/harkenscythe/entity/HSEntityGlobin.java
@@ -1,6 +1,7 @@
 package mod.emt.harkenscythe.entity;
 
 import net.minecraft.entity.monster.EntitySlime;
+import net.minecraft.item.Item;
 import net.minecraft.world.World;
 
 public abstract class HSEntityGlobin extends EntitySlime
@@ -20,5 +21,12 @@ public abstract class HSEntityGlobin extends EntitySlime
     protected void alterSquishAmount()
     {
         this.squishAmount *= 0.9F;
+    }
+
+    // Remove hardcoded slimeball drop
+    @Override
+    protected Item getDropItem()
+    {
+        return this.getSlimeSize() == 1 ? null : null;
     }
 }
