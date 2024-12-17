98fd2eaa3560e3055468692a0273ee6baa65137d
ACGaming <4818419+ACGaming@users.noreply.github.com>
2024-12-17 11:34:05 +0100

    Add cloth armor guidebook entry

--- src/main/java/mod/emt/harkenscythe/client/renderer/HSRendererEntitySpectralMiner.java
+++ src/main/java/mod/emt/harkenscythe/client/renderer/HSRendererEntitySpectralMiner.java
@@ -4,6 +4,7 @@ import net.minecraft.client.model.ModelPlayer;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.layers.LayerBipedArmor;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 import net.minecraftforge.fml.relauncher.Side;
@@ -21,6 +22,7 @@ public class HSRendererEntitySpectralMiner extends RenderBiped<HSEntitySpectralM
     public HSRendererEntitySpectralMiner(RenderManager renderManager)
     {
         super(renderManager, new ModelPlayer(0.0F, false), 0.5F);
+        this.addLayer(new LayerBipedArmor(this));
     }
 
     @Override
